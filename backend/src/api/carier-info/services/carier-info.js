cfe0bf89990138c7f830c361b324500fb68d19dc
DimonBel <dimabelih2004pro3@gmail.com>
2024-11-08 10:23:55 +0200

    Add schema for enrich datasets

---
+++ backend/src/api/carier-info/services/carier-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * carier-info service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::carier-info.carier-info');
