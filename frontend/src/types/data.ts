c5b3a0cbc6fe90b7a2820ec319f8169626ec5be0
Dmitrii <dimabelih2004pro3@gmail.com>
2025-01-23 11:09:13 +0200

    Modify Display Data

--- frontend/src/types/data.ts
+++ frontend/src/types/data.ts
@@ -1,20 +1,28 @@
-export interface DataItem {
-    id: string;
-    Name?: string;
-    Email?: string;
-    Phone?: string;
-    LinkedIn?: string;
-    [key: string]: any; // To accommodate dynamic fields like Enriched data
+export type DataItem ={
+    // id: string;
+    // Name?: string;
+    // Email?: string;
+    // Phone?: string;
+    // LinkedIn?: string;
+    // [key: string]: any; // To accommodate dynamic fields like Enriched data
+
+    id: number;
+    name: string;
+    value: string;
+    CarrierInfo?: string;
+    PhoneType?: string;
+    CountryInfo?: string;
+
 }
 
-export interface ColumnItem {
+export type ColumnItem ={
     title: React.ReactNode;
     dataIndex: string;
     key: string;
     width?: number;
     render?: (text: any) => React.ReactNode;
 }
-export interface TogetherData {
+export type TogetherData ={
     id: string | number;
     Name?: string;
     Email?: string;
